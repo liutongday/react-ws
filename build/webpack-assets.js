87bcef55d9c6931e819b3ce48ea3a9bc3f47b155
zs <1905777568@qq.com>
2016-08-12 10:15:33 +0800

    解决冲突

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.72857ff5fe5bd5602b5f.bundle.js"}}
-=======
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.56201aabba09a2eef90c.bundle.js"},"site":{"js":"/react-ws/build/site.56201aabba09a2eef90c.bundle.js"}}
->>>>>>> 00953b2a2e06c9970aff7ad5e62f4f836f1870eb
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.ec4897c56cb15fc8f3f6.bundle.js"},"site":{"js":"/react-ws/build/site.ec4897c56cb15fc8f3f6.bundle.js"}}
