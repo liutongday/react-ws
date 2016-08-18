61326b9eaf262fd0fca3a520dc1cc4b035c1c6e3
zs <1905777568@qq.com>
2016-08-18 15:22:21 +0800

    底部 ie 9 适应问题

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.1f58ac4e53a42e98ee05.bundle.js"},"site":{"js":"/react-ws/build/site.1f58ac4e53a42e98ee05.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.f575963ad118c36b7279.bundle.js"},"site":{"js":"/react-ws/build/site.f575963ad118c36b7279.bundle.js"}}
