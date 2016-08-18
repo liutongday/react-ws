23deea1495a1be636690fad26157f7f80b1cd29f
zs <1905777568@qq.com>
2016-08-18 16:40:44 +0800

    头部 ie 9 兼容

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.f575963ad118c36b7279.bundle.js"},"site":{"js":"/react-ws/build/site.f575963ad118c36b7279.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.19c9a68d30143ecec5af.bundle.js"},"site":{"js":"/react-ws/build/site.19c9a68d30143ecec5af.bundle.js"}}
