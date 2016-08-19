489697743959c88cd7f52801f2d65bcff3e731fe
zs <1905777568@qq.com>
2016-08-19 11:46:05 +0800

    修复ie9-11 谷歌 火狐 头部底部侧面导航栏兼容性问题

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.643c308cb411b741dd99.bundle.js"},"site":{"js":"/react-ws/build/site.643c308cb411b741dd99.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.c4b164cb0d5428ab66d8.bundle.js"},"site":{"js":"/react-ws/build/site.c4b164cb0d5428ab66d8.bundle.js"}}
