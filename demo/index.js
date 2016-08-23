d79e73b87a77512ee9b465768aeccbf8b57f77c3
潘先文 <panxianwen@bonc.com.cn>
2016-08-23 18:02:40 +0800

    react-router0.1

--- demo/index.js
+++ demo/index.js
@@ -21,58 +21,32 @@ import {
     Calendar,
     DatePicker,
 } from '../src/index';
-import _ from 'underscore';
+
 import Layout from './component/layout';
-import Form from './component/form';
 import Head from './component/head';
-import LeftNavigation from './component/LeftNavigation';
-import WsTable1 from './component/wsTableCol4';
-import WsTable2 from './component/wsTableCol3';
 import Apps from './component/apps';
 import Chart from './component/chart';
-import Map from './component/map';
-import CheckboxGroup from './component/CheckboxGroup';
-
-
 
 window.Storage = Storage;
-
-function initNav() {
-    let div = document.createElement('div');
-    div.className = 'doc-nav';
-
-    let html = '';
-    _.each(document.getElementsByTagName('h1'), ele => {
-        html += '<a class="doc-nav-title" href="#' + ele.id + '">' + ele.innerHTML + '</a>';
-        _.each(ele.parentNode.getElementsByTagName('h2'), e => {
-            html += '<a href="#' + e.id + '">' + e.innerHTML + '</a>';
-        });
-    });
-
-    div.innerHTML = html;
-    document.body.appendChild(div);
-}
 
 class App extends React.Component {
     render() {
         return (
             <div className="HolyGrail">
                 <header>Unicom Test</header>
-
                 <Head />
 
                 <div className="HolyGrail-body">
 
                     <nav className="HolyGrail-nav">
-                        <LeftNavigation />
+                        <nav>
+                            <Link to="/apps">Apps</Link>
+                            <Link to="/chart">Chart</Link>
+                        </nav>
                     </nav>
 
                     <div className="HolyGrail-content">
-                        <Apps />
-                        <Layout />
-                        <Calendar />
-                        <DatePicker />
-                        <Form />
+                        {this.props.children}
                     </div>
 
                 </div>
@@ -83,8 +57,21 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        //initNav();
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('appContainer'));
+class Pages extends React.Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="/" component={App}>
+                    <IndexRoute component={Layout}></IndexRoute>
+                    <Route path="apps" component={Apps}></Route>
+                    <Route path="chart" component={Chart}></Route>
+                </Route>
+            </Router>
+        );
+    }
+}
+
+ReactDOM.render(<Pages/>, document.getElementById('appContainer'));
