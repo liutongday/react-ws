6921c5b6aea992128d4c08fab3a2243d42436841
liutong <liutong@bonc.com.cn>
2016-08-24 16:52:25 +0800

    test

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.60e28641fdd4d3c4e2ae.bundle.js"},"site":{"js":"/react-ws/build/site.60e28641fdd4d3c4e2ae.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.0898d451faec42043382.bundle.js"},"site":{"js":"/react-ws/build/site.0898d451faec42043382.bundle.js"}}
