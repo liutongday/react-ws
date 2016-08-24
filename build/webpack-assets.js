66dfcbe51cc801e78c5570b40f6b5ca7aa0a2968
潘先文 <panxianwen@bonc.com.cn>
2016-08-24 16:22:22 +0800

    merge dev

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.05b092ea4b7a63bf3e9f.bundle.js"},"site":{"js":"/react-ws/build/site.05b092ea4b7a63bf3e9f.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.60e28641fdd4d3c4e2ae.bundle.js"},"site":{"js":"/react-ws/build/site.60e28641fdd4d3c4e2ae.bundle.js"}}
