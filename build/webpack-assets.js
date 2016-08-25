3b8d2fcd1b7b6e566e8e1e73953968458f59a4bd
liutong <liutong@bonc.com.cn>
2016-08-25 21:05:48 +0800

    merge liutong

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.76ec73df4ea22b87a01b.bundle.js"},"site":{"js":"/react-ws/build/site.76ec73df4ea22b87a01b.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.8d951db50ee097e21f51.bundle.js"},"site":{"js":"/react-ws/build/site.8d951db50ee097e21f51.bundle.js"}}
