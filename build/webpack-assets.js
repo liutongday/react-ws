bff0d1b826d59d3a17fed080702d638b4943cbf5
张鑫晶 <zhangxinjing@bonc.com.cn>
2016-08-30 10:43:38 +0800

    x

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.6ec9c997fe1401f7a470.bundle.js"},"site":{"js":"/react-ws/build/site.6ec9c997fe1401f7a470.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.6ab956bcfaa8d1d1f276.bundle.js"},"site":{"js":"/react-ws/build/site.6ab956bcfaa8d1d1f276.bundle.js"}}
