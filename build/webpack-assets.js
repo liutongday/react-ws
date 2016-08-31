b350c8296809344229b9954062c4eaa6c4dacd2c
zs <1905777568@qq.com>
2016-08-31 09:54:24 +0800

    。。。

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.20f2acafb95a88b21321.bundle.js"},"site":{"js":"/react-ws/build/site.20f2acafb95a88b21321.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.010dceedf891877feb1e.bundle.js"},"site":{"js":"/react-ws/build/site.010dceedf891877feb1e.bundle.js"}}
