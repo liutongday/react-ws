3ac6191d3dbb875a2bfac5e742b6a058e33e679e
wangpei <1161755451@qq.com>
2016-08-31 14:39:05 +0800

    .

--- build/webpack-assets.js
+++ build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.c75719683793c22193d4.bundle.js"},"site":{"js":"/react-ws/build/site.c75719683793c22193d4.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-ws/build/index.e430eab0ee418b3de5e0.bundle.js"},"site":{"js":"/react-ws/build/site.e430eab0ee418b3de5e0.bundle.js"}}
